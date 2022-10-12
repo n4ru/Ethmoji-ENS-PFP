24ebf53f1646d394af7e39682ee5cbe2a77645c4
John Doe <email@example.com>
2022-10-12 05:27:34 -0400

    made color an integer, should fix everything now

--- contracts/ENSNFT.sol
+++ contracts/ENSNFT.sol
@@ -1,9 +1,81 @@
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+//import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+/**
+ * @dev String operations.
+ */
+library StringsB {
+    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+    uint8 private constant _ADDRESS_LENGTH = 20;
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
+     */
+    function toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT licence
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
+    }
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation.
+     */
+    function toHexString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0x00";
+        }
+        uint256 temp = value;
+        uint256 length = 0;
+        while (temp != 0) {
+            length++;
+            temp >>= 8;
+        }
+        return toHexString(value, length);
+    }
+
+    /**
+     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
+     */
+    function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = _HEX_SYMBOLS[value & 0xf];
+            value >>= 4;
+        }
+        require(value == 0, "Strings: hex length insufficient");
+        return string(buffer);
+    }
+
+    /**
+     * @dev Converts an `address` with fixed length of 20 bytes to its not checksummed ASCII `string` hexadecimal representation.
+     */
+    function toHexString(address addr) internal pure returns (string memory) {
+        return toHexString(uint256(uint160(addr)), _ADDRESS_LENGTH);
+    }
+}
+
 
 struct Domain {
     address owner;
@@ -382,7 +454,7 @@ contract EmojiNFT is ERC721 {
         User memory user = storageContract.getUser(msg.sender);
         Domain memory domain = storageContract.getDomain(tokenId);
         require(user.current != tokenId && domain.owner != msg.sender, "You're already displaying this domain.");
-        //require(collection.ownerOf(tokenId) == msg.sender, "You don't own the domain.");
+        require(collection.ownerOf(tokenId) == msg.sender, "You don't own the domain.");
         uint256 mintFee = (isPure ? purefee : (keycaps > 0 ? keycapfee / keycaps : fee));
         require((user.feePaid + msg.value) >= mintFee, "fee too low");
         if (user.current == 0) supply++; // Increase supply if this is the wallet's first mint
@@ -410,6 +482,7 @@ contract EmojiNFT is ERC721 {
         require(custom[node] == true, "not allowed to set color");
         address user = resolve(node);
         require(user == msg.sender, "user can only change their color");
+        require(_color < 16777216 && _color > 0); // #000000 - #FFFFFF
         storageContract.setColor(msg.sender, _color);
     }
 
@@ -422,7 +495,7 @@ contract EmojiNFT is ERC721 {
         Domain memory domain = storageContract.getDomain(tokenId);
         User memory user = storageContract.getUser(domain.owner);
         bool stillOwned = (collection.ownerOf(tokenId) == domain.owner);
-        (string memory display, uint256 node, uint256 keycaps, bytes memory parsed,,bool isPure) = verify.test(domain.name);
+        (string memory display,, uint256 keycaps, bytes memory parsed, bytes32 node, bool isPure) = verify.test(domain.name);
         // Get total number of emojis
         uint256 num = 0;
         uint256 emojiSize = user.size;
@@ -432,9 +505,8 @@ contract EmojiNFT is ERC721 {
         string memory color = "grey"; // Regular
         if (isPure) color = "gold"; // Pure
         if (keycaps > 0) color = "lightblue"; // Keycaps
-        if (user.color > 0 && (resolve(bytes32(node)) == domain.owner)) color = Strings.toString(user.color); // hack the planet!
+        if (user.color > 0 && (resolve(node) == domain.owner)) color = StringsB.toHexString(user.color, 3); // hack the planet!
         if (!stillOwned) color = "black"; // Not owned by minter anymore
-        
         for (uint256 i = 0; i < len; i++) num = num + uint8(parsed[i * 4]); // Add up all the emojis
         uint256 px = emojiSize / num;  // Initial font size (32-256) divided by the total number of emojis
 
@@ -443,7 +515,7 @@ contract EmojiNFT is ERC721 {
             "{\"name\": \"",
             domain.name, // ENS Domain (Normalized)
             "\", \"description\": \"An ENS Emoji Domain NFT\", \"image_data\": \"",
-            "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><rect width='100%' height='100%' fill='",
+            "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><rect width='100%' height='100%' fill='#",
             color, // Color
             "' /><text x='50%' y='55%' style='font-size: ",
             (Strings.toString(isPure ? emojiSize : px)),
